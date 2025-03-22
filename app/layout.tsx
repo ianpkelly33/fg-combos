d14a0e93d9beac1100362f20c12206eb429a088a
ianpkelly33 <ianpk33@gmail.com>
2025-03-22 16:47:45 -0700

    sticking with nextjs, angular too much for me lol

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,13 +1,17 @@
 import '@/app/ui/global.css';
+import Navbar from '@/app/ui/navbar';
 
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
+          <div className="w-full flex-none md:w-64">
+            <Navbar />
+          </div>
+          <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
+        </div>
+      </body>
     </html>
   );
 }
